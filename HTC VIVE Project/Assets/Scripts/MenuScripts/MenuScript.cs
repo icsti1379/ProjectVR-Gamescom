a5a82da680db551fc87faa48b0716f2f27799346
icsti1379 <it.icsti@googlemail.com>
2017-01-30 17:23:40 +0100

    Updated MenuScene

--- HTC VIVE Project/Assets/Scripts/MenuScripts/MenuScript.cs
+++ HTC VIVE Project/Assets/Scripts/MenuScripts/MenuScript.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using XInputDotNetPure;
+using System.Collections;
 
 public class MenuScript : MonoBehaviour
 {
     //TODO: Before finish delete all debug comments!
+    //TODO: Delete options menu if not needed
 
     /// <summary>
     /// Enum of all menu states.
@@ -16,35 +15,33 @@ public class MenuScript : MonoBehaviour
     {
         Main,
         Player,
+        //Options,
         Help,
-        Credits,
-        Exit
+        Highscore,
+        Credits
     }
 
     #region variables
+    //TODO: Delete options menu if not needed
 
     // Variable for define current state.
     public MenuStates currentState;
 
-    public GamePad gamePad;
-
     // Menu panel objects as GameObjects.
     public GameObject mainMenu;
     public GameObject playerMenu;
+    //public GameObject optionsMenu;
     public GameObject helpMenu;
+    public GameObject highscoreMenu;
     public GameObject creditsMenu;
-    //TODO: Uncomment this if application exit in MenuState.Exit works!
-    //public GameObject exitMenu;
-
+    
     // Player menu toggles
     public bool isSelected;
-    public Toggle isOnePlayer;
-    public Toggle isTwoPlayer;
-    public Toggle isThreePlayer;
-
+    public Toggle IsTutorialToggle;
+    
     #endregion
-    
-    
+
+
     /// <summary>
     /// Initialize any variables or game states before the game starts.
     /// Is called only once during the lifetime of the script instance.
@@ -62,55 +59,69 @@ public class MenuScript : MonoBehaviour
     /// </summary>
     void Update()
     {
-        
+        //TODO: Delete options menu if not needed
 
         // Checks current menu state
         switch (currentState)
         {
-                case MenuStates.Main:
-                    // Sets active gameobject for main menu
-                    mainMenu.SetActive(true);
-                    playerMenu.SetActive(false);
-                    helpMenu.SetActive(false);
-                    creditsMenu.SetActive(false);
-                    //exitMenu.SetActive(false);
-                    break;
-
-                case MenuStates.Player:
-                    // Sets active gameobject for player menu
-                    playerMenu.SetActive(true);
-                    mainMenu.SetActive(false);
-                    helpMenu.SetActive(false);
-                    creditsMenu.SetActive(false);
-                    //exitMenu.SetActive(false);
-                    break;
-
-                case MenuStates.Help:
-                    // Sets active gameobject for options menu
-                    helpMenu.SetActive(true);
-                    mainMenu.SetActive(false);
-                    playerMenu.SetActive(false);
-                    creditsMenu.SetActive(false);
-                    //exitMenu.SetActive(false);
-                break;
-
-                case MenuStates.Credits:
-                    // Sets active gameobject for options menu
-                    creditsMenu.SetActive(true);
-                    mainMenu.SetActive(false);
-                    playerMenu.SetActive(false);
-                    helpMenu.SetActive(false);
-                    //exitMenu.SetActive(false);
-                break;
-
-            case MenuStates.Exit:
-                //Exits game
-                //mainMenu.SetActive(false);
-                //playerMenu.SetActive(false);
+            case MenuStates.Main:
+                // Sets active gameobject for main menu
+                mainMenu.SetActive(true);
+                playerMenu.SetActive(false);
                 //optionsMenu.SetActive(false);
-                //helpMenu.SetActive(false);
-                //creditsMenu.SetActive(false);
-                //exitMenu.SetActive(true);
+                helpMenu.SetActive(false);
+                highscoreMenu.SetActive(false);
+                creditsMenu.SetActive(false);
+                break;
+
+            case MenuStates.Player:
+                // Sets active gameobject for player menu
+                playerMenu.SetActive(true);
+                mainMenu.SetActive(false);
+                //optionsMenu.SetActive(false);
+                helpMenu.SetActive(false);
+                highscoreMenu.SetActive(false);
+                creditsMenu.SetActive(false);
+                break;
+
+            //case MenuStates.Options:
+            //    // Sets active gameobject for options menu
+            //    //optionsMenu.SetActive(true);
+            //    mainMenu.SetActive(false);
+            //    playerMenu.SetActive(false);
+            //    helpMenu.SetActive(false);
+            //    highscoreMenu.SetActive(false);
+            //    creditsMenu.SetActive(false);
+            //    break;
+
+            case MenuStates.Help:
+                // Sets active gameobject for help menu
+                helpMenu.SetActive(true);
+                mainMenu.SetActive(false);
+                playerMenu.SetActive(false);
+                //optionsMenu.SetActive(false);
+                highscoreMenu.SetActive(false);
+                creditsMenu.SetActive(false);
+                break;
+
+            case MenuStates.Highscore:
+                // Sets active gameobject for highscore menu
+                highscoreMenu.SetActive(true);
+                mainMenu.SetActive(false);
+                playerMenu.SetActive(false);
+                //optionsMenu.SetActive(false);
+                helpMenu.SetActive(false);
+                creditsMenu.SetActive(false);
+                break;
+
+            case MenuStates.Credits:
+                // Sets active gameobject for options menu
+                creditsMenu.SetActive(true);
+                mainMenu.SetActive(false);
+                playerMenu.SetActive(false);
+                //optionsMenu.SetActive(false);
+                helpMenu.SetActive(false);
+                highscoreMenu.SetActive(false);
                 break;
         }
     }
@@ -125,13 +136,7 @@ public class MenuScript : MonoBehaviour
     {
         // Log activity
         Debug.Log("You pressed start game!");
-
-        //TODO: If LoadScene is being used delete this.
         currentState = MenuStates.Player;
-
-        //TODO: Uncomment this if player selection after start is not needed and is implemented else.
-        // Add load level for new scene here.
-        // SceneManager.LoadScene("GameScreen");
     }
 
     /// <summary>
@@ -149,15 +154,18 @@ public class MenuScript : MonoBehaviour
         // Add load level for new scene here.
         if (isSelected == true)
         {
-            SceneManager.LoadScene("Main_Game_V1");
+            // Log activity
+            Debug.Log("Load TutorialScene");
+
+            SceneManager.LoadScene("TutorialScene");
         }
 
         else
         {
             // Log activity
-            Debug.Log("SELECT ONE OPTION!!!");
-
-            //TODO: Add function to deactivate button visualy maybe?
+            Debug.Log("Load GameScene");
+            
+            SceneManager.LoadScene("GameScene");
         }
     }
 
@@ -189,6 +197,25 @@ public class MenuScript : MonoBehaviour
         currentState = MenuStates.Help;
     }
 
+    /// <summary>
+    /// Interaction with highscore button.
+    /// Changes current state to highscoreMenu.
+    /// </summary>
+    public void OnHighscore()
+    {
+        // Log activity
+        Debug.Log("Highscore button clicked.");
+
+        
+
+        // Change menu state
+        currentState = MenuStates.Highscore;
+    }
+
+    /// <summary>
+    /// Interaction with credits button.
+    /// Changes current state to creditsMenu.
+    /// </summary>
     public void OnCredits()
     {
         // Log activity
@@ -198,68 +225,27 @@ public class MenuScript : MonoBehaviour
         currentState = MenuStates.Credits;
     }
 
-    public void OnExit()
-    {
-        // Log activity
-        Debug.Log("Ext button clicked.");
-
-        Application.Quit();
-
-        //TODO: Menu state or just Application.Quit() ??
-        // Change menu state
-        //currentState = MenuStates.Exit;
-    }
-
     #endregion
 
     #region Player menu toggles
 
     /// <summary>
-    /// Checks which toggle is active.
+    /// Checks if toggle is active.
     /// </summary>
     public void ActiveToggle()
     {
-        if (isOnePlayer.isOn)
+        if (IsTutorialToggle.isOn)
         {
             // Log activity
-            Debug.Log("One player selected!");
-            isSelected = true;
-        }
-        
-        else if (isTwoPlayer.isOn)
-        {
-            // Log activity
-            Debug.Log("Two players selected!");
+            Debug.Log("Play tutorial first is selected.");
             isSelected = true;
         }
 
-        else if (isThreePlayer.isOn)
-        {
-            // Log activity
-            Debug.Log("Three players selected!");
-            isSelected = true;
-        }
         else
         {
-            // Log activity
-            Debug.Log("No option is selected...");
             isSelected = false;
         }
     }
-
-    #endregion
-
-    // If needed for event handling
-    #region EventHandler
-
-    //NOTE: ClickHandler maybe for exit application?! -> Just mouse ...
-    //public void OnPointerClick(PointerEventData eventData)
-    //{
-    //    if (eventData.button == PointerEventData.InputButton.Left)
-    //    {
-    //        Application.Quit();
-    //    }
-    //}
 
     #endregion
 }
